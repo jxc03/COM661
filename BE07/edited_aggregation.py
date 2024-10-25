0a3ee3939c58ffb72c7bec030c6ea14e4dfc8926
Jonnie <Carino-j1@ulster.ac.uk>
2024-10-25 15:18:07 +0100

    Aggregation bits

--- BE07/edited_aggregation.py
+++ BE07/edited_aggregation.py
@@ -5,10 +5,10 @@ db = client.bizDB
 businesses = db.biz
 
 pipeline = [
-    {"$match" : {"town" : "Belfast"}},
-    {"$project" : {"name" : 1, "num_employees" : 1 }},
-    {"$unwind" : {"$profit"}}
+    {"$match": {"town": "Belfast"}},
+    {"$project": {"name": 1, "num_employees" : 1, "_id": 0 }}
+    #{"$unwind" : {"$profit"}}
     ]
 
 for business in businesses.aggregate(pipeline):
-    print(business["town"], str(business["profit"][2]["gross"]))
+    print(f"Business name: {business["name"]}, Number of employees: {business["num_employees"]}")
