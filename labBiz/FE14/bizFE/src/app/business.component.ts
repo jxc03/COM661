b413e4b56a81baa7e8fbfb9e105e64f881f97997
Jonnie <jcarino2003@hotmail.com>
2024-11-15 16:09:43 +0000

    Needs fixing, nearly finsihed

--- labBiz/FE14/bizFE/src/app/business.component.ts
+++ labBiz/FE14/bizFE/src/app/business.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet, ActivatedRoute } from '@angular/router';
 import { DataService } from './data.service';
 import { CommonModule} from '@angular/common';
 import { GoogleMapsModule } from '@angular/google-maps';
+import { subscribe } from 'diagnostics_channel';
 
 @Component({
   selector: 'business',
@@ -19,17 +20,23 @@ export class BusinessComponent {
   business_lng: any;
   map_options: google.maps.MapOptions = {};
   map_locations: any[] = [];
+  loremIpsum: any;
+  temperature: any;
+  weather: any;
+  weatherIcon: any;
+  weatherIconURL: any;
+  temperatureColour: any;
 
   constructor(public dataService: DataService, 
               private route: ActivatedRoute) {}
 
-ngOnInit() {
+  ngOnInit() {
     this.business_list = this.dataService.getBusiness(
                             this.route.snapshot.paramMap.get('id')
                           );
-    this.business_lat = this.business_list[0].location.coordinates[0]; 
-    this.business_lng = this.business_list[0].location.coordinates[1];
-    this.map_locations.push({
+    /*this.business_lat = this.business_list[0].location.coordinates[0];*/
+    /*this.business_lng = this.business_list[0].location.coordinates[1];*/
+    /* this.map_locations.push({
       lat: this.business_lat, lng: this.business_lng
     });
     this.map_options = {
@@ -39,6 +46,16 @@ ngOnInit() {
         lng: this.business_lng
       },
       zoom: 13,
-    };
+    }; */
+    this.dataService.getLoremIpsum(1).subscribe((response: any) => {this.loremIpsum = response.text.slice(0, 400);})
+    
+    this.dataService.getCurrentWeather(this.business_lat, this.business_lng).subscribe((response: any) => {
+      let weatherResponse = response['weather'][0]['description'];
+      this.temperature = Math.round((response['main']['temp']));
+      this.weather = weatherResponse[0].toUpperCase() + weatherResponse.slice(1);
+      this.weatherIcon = response['weather'][0]['icon'];
+      this.weatherIconURL = "https://openweathermap.org/img/wn/" + this.weatherIcon + "@4x.png";
+      this.temperatureColour = this.dataService.getTemperatureColour(this.temperature)
+    });
   }
 }
--- labBiz/FE14/bizFE/src/app/data.service.ts
+++ labBiz/FE14/bizFE/src/app/data.service.ts
@@ -1,7 +1,15 @@
 import jsonData from '../assets/data/bizDB.biz.json'
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({providedIn: "root"})
 
 export class DataService {
     pageSize: number = 6;
+
+    constructor(private http: HttpClient) {}
+
     getBusinesses(page: number) {
         let pageStart = (page - 1) * this.pageSize;
         let pageEnd = pageStart + this.pageSize;
@@ -18,5 +26,27 @@ export class DataService {
             }
         })
         return dataToReturn
+    }
+    getLoremIpsum(paragraphs: number): Observable<any> {
+        let API_key = 'MIJrIqdjoBKYip+6+TKSrw==zaZWmjajV4aHTqpn';
+        return this.http.get<any>(
+        'https://api.api-ninjas.com/v1/' +
+        'loremipsum?paragraphs=' + paragraphs,
+        { headers: { 'X-Api-Key': API_key } }
+        );
+    }
+    getCurrentWeather(lat: number, lon: number) {
+        let API_key = "b13c69eca47b941b84b44306ea24c081";
+        return this.http.get<any>(
+        'https://api.openweathermap.org/data/2.5/weather?lat=' + 
+        lat + '&lon=' + lon + '&units=metric&appid=' + API_key
+        );
+    }
+    getTemperatureColour(temp: number) {
+        if (temp <= 5) return "#0000ff";
+        else if (temp <= 12) return "#00ff00";
+        else if (temp <= 17) return "#ffff00";
+        else if (temp <= 25) return "#ff7f00";
+        else return "#ff0000"
     }
 }
