25ba8762ffbda4e1a8b7a3cb4570800933a40f0d
Jonnie <jcarino2003@hotmail.com>
2024-11-19 12:55:45 +0000

    Businesses component

---
+++ foodPlaces/frontend/foodPlacesFE/src/app/businesses.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { DataService } from './data.service';
+
+@Component({
+    selector: 'businesses',
+    standalone: true,
+    imports: [RouterOutlet],
+    providers: [DataService],
+    templateUrl: './businesses.component.html',
+    styleUrl: './businesses.component.css'
+})
+
+export class BusinessesComponent {
+    business_list: any;
+    constructor(private dataService: DataService) { }
+    ngOnInit() {
+    this.business_list = this.dataService.getBusinesses();
+    }
+}
