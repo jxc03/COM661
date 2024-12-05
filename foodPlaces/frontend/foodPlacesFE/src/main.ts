1d56df9b3e572d3174cfcfbb6da720087ccb1eba
Jonnie <jcarino2003@hotmail.com>
2024-12-05 11:32:44 +0000

    Changes for Auth0

--- foodPlaces/frontend/foodPlacesFE/src/main.ts
+++ foodPlaces/frontend/foodPlacesFE/src/main.ts
@@ -1,6 +1,24 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideAuth0 } from '@auth0/auth0-angular';
+import { provideHttpClient } from '@angular/common/http';
 
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideAuth0({
+      domain: 'dev-tnafsghc1wquofma.us.auth0.com',
+      clientId: 'Ii0Iz28p8KliUBpRe2CI8xaD49hMgUDE',
+      authorizationParams: {
+        redirect_uri: window.location.origin
+      }
+    }),
+  ]
+});
+
+/*
 bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+.catch((err) => console.error(err));
+*/
+
+
