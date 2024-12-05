93ffe7b7ac7f04bcd7e174dc5d0861e0052657a3
Jonnie <jcarino2003@hotmail.com>
2024-12-05 11:43:47 +0000

    home ts

---
+++ foodPlaces/frontend/foodPlacesFE/src/app/authbutton.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+
+@Component({
+    selector: 'auth-button',
+    templateUrl: 'authbutton.component.html',
+    standalone: true
+})
+
+export class AuthButtonComponent {
+    constructor(public auth: AuthService) {}
+}
